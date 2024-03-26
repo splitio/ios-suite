68330c8bd3d5b03331dd4624397045605fdae896
Javier L. Avrudsky <javier.avrudsky@split.io>
2024-03-26 14:30:52 -0300

    Release 0.1.0-rc2

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+
+let package = Package(
+    name: "SplitSuite",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "SplitSuite",
+            targets: ["iOSSplitSuite"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "iOSSplitSuite",
+            url: "https://cdn.split.io/ios-suite/iOSSplitSuite_0.1.0-rc2.zip",
+            checksum: "b08a62bec39d1793711d918c2c1b0009284cffeddb35dd45d4fcf895e12d06ec"
+        ),
+    ]
+)
