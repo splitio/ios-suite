9669a894748a5280f9aab48565029b4eb670b746
Javier L. Avrudsky <javier.avrudsky@split.io>
2024-03-27 08:52:37 -0300

    Release 1.0.0-rc1

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "iOSSplitSuite",
-            url: "https://cdn.split.io/ios-suite/iOSSplitSuite_0.1.0-rc2.zip",
+            url: "https://cdn.split.io/ios-suite/iOSSplitSuite_1.0.0-rc1.zip",
             checksum: "b08a62bec39d1793711d918c2c1b0009284cffeddb35dd45d4fcf895e12d06ec"
         ),
     ]
