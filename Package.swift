f8500a42775c4f8abe6670b877ba110f8f8e4da1
Gaston Thea <gaston.thea@split.io>
2024-05-23 18:05:52 -0300

    Update checksum

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .binaryTarget(
             name: "iOSSplitSuite",
             url: "https://cdn.split.io/ios-suite/iOSSplitSuite_1.2.0.zip",
-            checksum: "e31069378aeea4e40556f031dbe5c36af191ff323076b739c032baacb223875e"
+            checksum: "287f59aef802f0995a45ce29a1c97a6c47bc42cc62a341adf404fb3afaec8d45"
         ),
     ]
 )
