72001298a27ba02c9f561ecb5f7bf7f2b3d9eacb
Gaston Thea <gaston.thea@harness.io>
2025-05-22 12:52:14 -0300

    Version 2.2.0-rc1

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "iOSSplitSuite",
-            url: "https://cdn.split.io/ios-suite/iOSSplitSuite_2.1.0.zip",
-            checksum: "695351185eb14438399e4f6d32cbee30fefdc9c3fcda566ba2f833b4c2c3696c"
+            url: "https://cdn.split.io/ios-suite/iOSSplitSuite_2.2.0-rc1.zip",
+            checksum: "b34812f8546185127f0088e23376374f4df352acfb980b7e3e94a1f96222bc7a"
         ),
     ]
 )
