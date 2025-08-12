75713d2eccfc1e1c7af2e909de6d7fd87a8eef56
gthea <gthea@users.noreply.github.com>
2025-08-12 15:04:15 +0000

    Update Suite version to 2.2.2

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "iOSSplitSuite",
-            url: "https://cdn.split.io/ios-suite/iOSSplitSuite_2.2.1.zip",
-            checksum: "86ecd4081fdb1d0f8b53cd939430d96280b37f900160d02d84b5377cdce48522"
+            url: "https://cdn.split.io/ios-suite/iOSSplitSuite_2.2.2.zip",
+            checksum: "be09e8a82851331dfefe47888b0955733737cae015d3f83e15cfca75ebac0527"
         ),
     ]
 )
