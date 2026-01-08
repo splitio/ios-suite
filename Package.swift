06944c93b7793602775bd88ee2af5ed6fb20dd23
MartinCardozo-SDK <MartinCardozo-SDK@users.noreply.github.com>
2026-01-08 22:01:10 +0000

    Update Suite version to 2.5.0

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "iOSSplitSuite",
-            url: "https://cdn.split.io/ios-suite/iOSSplitSuite_2.4.2.zip",
-            checksum: "606c6d4c5bfd121b3b91a2dee00894e8b9d32a0bc673bc1182bb3fdcb090e517"
+            url: "https://cdn.split.io/ios-suite/iOSSplitSuite_2.5.0.zip",
+            checksum: "d86c3a5bfaadbcc987d37e2c7e65292900f008b4e3d6b0d4b04bdf7a0ce8eddd"
         ),
     ]
 )
